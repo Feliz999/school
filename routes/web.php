918f8674bfb9bf5f4d2a01f6c6facab2ae25a6a6
Allan Ramirez <allanramirez.edutec@gmail.com>
2022-09-23 12:53:53 -0600

    se agregaron las carpetas para estilo, funciones e imagenes, configuraciones de inicio y el redireccionamiento de las rutas, se agregaron las rutas de las nuevas tablas creadas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::resource('typePeople','TypePeopleController');
+    Route::resource('typeUser','TypeUserController');
+    Route::resource('typeHomework','TypeHomeworkController');
+    Route::resource('ciclo','CicloController');
+    Route::resource('level','LevelController');
+    Route::resource('matter','MatterController');
+    Route::resource('section','SectionController');
+    Route::resource('student','StudentController');
+    Route::resource('teacher','TeacherController');
+    Route::resource('people','PeopleController');
+    Route::resource('homework','HomeworkController');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
